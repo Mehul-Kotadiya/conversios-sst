b19c6fc1e2caaec22e5653c07d0fd20215087237
Jeet Shah <jeet.shah@tatvic.com>
2023-09-06 08:46:41 +0000

    Update file create.py

--- create.py
+++ create.py
@@ -1,14 +1,14 @@
 from google.cloud import run_v2
 import os
+import re
 
 # project_id="server-side-tagging-392006"
-#location="asia-south1"
-
+# location="asia-south1"
 # GOOGLE_APPLICATION_CREDENTIALS="/home/jeet/internal/Conversios/development/creds.json"
 project_id=os.getenv("PROJECT_ID")
 location=os.getenv("LOCATION")
 name=os.getenv("NAME")
-# name="dem4"
+name="dem66"
 # GOOGLE_APPLICATION_CREDENTIALS=os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/jeet/internal/Conversios/development/creds.json"
 
@@ -53,16 +53,21 @@ def create_service_preview_tagging():
     operation = run_client.create_service(metadata= [("name", "sst-"+name)],request=request)
     print("Waiting for operation to complete...")
     response = operation.result()
-    preview_url=response.uri
-    print(preview_url)
-    container_config=response.template.containers.env[0].value
-    print(container_config)
+    preview_url_value=response.uri
+    # print(preview_url_value)
     
+    cont=str(response.template.containers)
+    match = re.search(r'name: "CONTAINER_CONFIG"\s+value: "(.*?)"', cont)
+    if match:
+        container_config_value = match.group(1)
+        # print(container_config_value)
+    else:
+        print("CONTAINER_CONFIG not found in the input string.")
     
-create_service_preview_tagging()
+    return preview_url_value,container_config_value
 
 
-def create_service_tagging():
+def create_service_tagging(par1,par2):
     run_client = run_v2.ServicesClient()
 
     request = run_v2.CreateServiceRequest(
@@ -86,10 +91,10 @@ def create_service_tagging():
                     },
                     "env": [{
                         "name": "CONTAINER_CONFIG",
-                        "value": "aWQ9R1RNLU5EWlRTRE0mZW52PTEmYXV0aD1ma0hfRHNyOWk1YmZQT1dvSGxyY0Zn"
+                        "value": par2
                         },
                         {"name": "PREVIEW_SERVER_URL",
-                        "value": "https://sst-test-preview-nszpmsl3ca-el.a.run.app",
+                        "value": par1,
                     }]
                     
                 }],
@@ -105,4 +110,8 @@ def create_service_tagging():
     response = operation.result()
     print("tagging server",response)
     
-create_service_tagging()
+
+result1,result2=create_service_preview_tagging()
+print(result1,result2)
+
+create_service_tagging(result1,result2)

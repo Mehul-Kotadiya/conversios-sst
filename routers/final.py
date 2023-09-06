fe00a8c50012aeb004bf5d7418b8198a21a75729
Jeet Shah <jeet.shah@tatvic.com>
2023-09-06 14:18:27 +0000

    Added IAM

--- routers/final.py
+++ routers/final.py
@@ -5,6 +5,7 @@ import uvicorn
 import os 
 import re
 from google.cloud import run_v2
+from google.iam.v1 import iam_policy_pb2 
 
 project_id="server-side-tagging-392006"
 
@@ -23,8 +24,10 @@ async def sst_create(request: Request):
     print("name",store_id)
     print("region",region)
     print("container_config",container_config)
-    preview_server_url=await create_service_preview_tagging(store_id,region,container_config)
-    result1=await create_service_tagging(store_id,region,container_config,preview_server_url)
+    preview_server_url,preview_name=await create_service_preview_tagging(store_id,region,container_config)
+    sample_set_iam_policy(preview_name)
+    result1,tagging_name=await create_service_tagging(store_id,region,container_config,preview_server_url)
+    sample_set_iam_policy(tagging_name)
     return {"server_url": result1}
 
 # location="asia-south1"
@@ -75,6 +78,8 @@ async def create_service_preview_tagging(store_id: str,region:str,container_conf
     print("Setting up preview server")
     response = operation.result()
     preview_url_value=response.uri
+    nx=(str(response.name))
+    print("Name of full server name :",nx)
     # print(preview_url_value)
     print("Preview_Server Response:",response)
     
@@ -86,7 +91,7 @@ async def create_service_preview_tagging(store_id: str,region:str,container_conf
     # else:
     #     print("CONTAINER_CONFIG not found in the input string.")
     
-    return preview_url_value
+    return preview_url_value,nx
 
 
 async def create_service_tagging(store_id,region,container_config,preview_server_url):
@@ -131,12 +136,30 @@ async def create_service_tagging(store_id,region,container_config,preview_server
     print("Setting up server side tagging...")
     response = operation.result()
     server_url_value=response.uri
+    nx=(str(response.name))
+    print("Name of full server name :",nx)
     nt=(str(response.name)).split("services/")[1]
     print("Name of server:",nt)
     print("Server_url",server_url_value)
     print("Tagging server:",response)
-    return server_url_value,nt
+    return server_url_value,nx
 
+def sample_set_iam_policy(resource):
+    client = run_v2.ServicesClient()
+    request = iam_policy_pb2.SetIamPolicyRequest(
+        # resource=f"projects/{project_id}/locations/{location}/service/sst-dem66",
+        resource=resource,
+        policy={
+            "bindings": [
+                {
+                    "role": "roles/run.invoker",
+                    "members": ["allUsers"]
+                }
+                        ]
+                }
+        )
 
-if __name__ == "__main__":
-    uvicorn.run("test:app", host="127.0.0.1", port=8000,reload=True)
+    # Make the request
+    response = client.set_iam_policy(request=request)
+    print(response)
+

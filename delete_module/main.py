861206775623faa4db4de13ad4e20605293fe402
mehul <mehul@tatvic.com>
2023-11-24 17:10:02 +0530

    testing for delete module

--- delete_module/main.py
+++ delete_module/main.py
@@ -3,6 +3,7 @@ import os
 from google.cloud import pubsub_v1
 import configparser
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from google.cloud import datastore
@@ -52,17 +53,12 @@ project = "server-side-tagging-392006"
 subscription_name='server-side-tagging-topic-sub'
 subscriber = pubsub_v1.SubscriberClient()
 
-# task_key = client.key('server-side-tagging', store_id)
-# task = client.get(task_key)
-
-@app.get("/")
+@app.get("/batch_delete")
 async def batch_function ():
 
     
     config = configparser.ConfigParser()
     config.read('config.ini')
-    # ssl_delete(certificate_name='sst-10002-certificate-1700123575381')
-    # exit()
 
     subscription_path = subscriber.subscription_path(project, subscription_name)
 
@@ -72,175 +68,178 @@ async def batch_function ():
 
     for received_message in response.received_messages:
         try:
+            print(received_message)
             print(f"Received message: {received_message.message.data}")
             store_id.append(received_message.message.data.decode('utf-8'))
+            ack=received_message.ack_id
+            print(ack)
+            # exit()
             # subscriber.acknowledge(
-            # subscription=subscription_path,
+            # subscription=subscrack=received_message.ack_idiption_path,
             # ack_ids=[received_message.ack_id],
-            # )        
+            # )          
+            certificate_99,latest_certificate,remaining_certificate = DomainList.domain_list()
+            print('99 certi',certificate_99)
+            print('latest_time',latest_certificate)
+            print('remaining_certificate',remaining_certificate)
+            delete_certi=remaining_certificate[0].split("/")[-1]
+            patch_require_certi.append(certificate_99)
+            patch_require_certi.append(latest_certificate)
+            print('delete required',delete_certi)
+                   
+            
+            print('final certi status check',latest_certificate.split("/")[-1])    
+            certi_status = get_ssl_certi(certificate_name=latest_certificate.split("/")[-1])
+            print(certi_status)
+            
+            if certi_status == 'PROVISIONING':
+                finger_print=https_proxy_get(load_balancer=lb)
+                print('get proxy')
+                patch_lb_front_end(load_balancer=lb,fingerprint=finger_print[0])
+                print('update proxy')
+                time.sleep(5)
+                
+                
+                urlmap_get(lb=lb) #give static lb name
+                print('url get and update path')
+                time.sleep(5)
+                get_backend_service()
+                print('get backend')
+                time.sleep(10)
+                backend_delete(backend_service_name=be_name[0])
+                time.sleep(10)
+                print('delete backend')
+                get_neg_list()
+                time.sleep(10)
+                print('get neg')
+
+                neg_delete()
+                time.sleep(10)
+                print('delete neg')
+                ssl_delete(certificate_name=delete_certi)
+                print('All items deleted sucessfully')
+                print("pointer",received_message)
+                # print("mid",received_message['message']['message_id'])
+                # print("mid2",received_message.message.message_id)
+                
+                res=subscriber.acknowledge(
+                subscription=subscription_path,
+                ack_ids=[ack],
+                    )     
+                print('pubsub res',res)
+            else:
+                print('The new certificate is not Active yet ')
+
+            content = {"message": "complete the scan"}
+            return JSONResponse(content=content, status_code=200)
         except Exception as e:
             print(f"Error processing or acknowledging the message: {e}")
 
-    # exit()
 
-    # ----------------------------- pubsub part
-  
-    # client = compute_v1.SslCertificatesClient()
-    # ssl_certificates = client.list(project=project)
-    # print('---------------------------------------------------')
-    
-    
-   
-    # json_formatted_data = []
-    
+''' Delete the perivous certificate'''
 
-    # for cert in ssl_certificates:
+@app.get("/create-delete")
 
-    #         certificate_data = {
-    #             'name': cert.name,
-    #             'status': cert.managed.status,
-    #             'create_time': cert.creation_timestamp,
-    #         }
-    #         json_formatted_data.append(certificate_data)
-    #         print(store_id[0])
-    #         if str(store_id[0]) in cert.name: #set store id from datastore update part
-    #              length_check.append(cert.name)
-                 
-            
+def create_delete_batch():
 
-
-    # print('check-length',len(length_check))
-    # if len(length_check) == 2:
-    #     #  print('now ready to run code')
-    #     for cert in json_formatted_data:
-    #         if str(store_id[0]) in cert['name']:
-    #             name_data={
-    #                 "name":cert['name'],
-    #                 "status":cert['status'],
-    #                 "create_time":cert['create_time']
-    #             }
-    #             print('**********')
-                
-                    
-    #             name.append(name_data)
-    #     # print(name)
-    #     if name[0]['create_time'] < name[1]['create_time']:
-    #         final_cert_name.append(name[0]['name'])
-    #         final_status_check_cert.append(name[1]['name'])
-    #     else:
-    #         final_cert_name.append(name[1]['name'])
-    #         final_status_check_cert.append(name[0]['name'])
-    #     # print('--------')
-    #     print('cert name',final_cert_name)
-    #     print('status check',final_status_check_cert)
+    full_url_certi = []
     certificate_99,latest_certificate,remaining_certificate = DomainList.domain_list()
-    print('99 certi',certificate_99)
-    print('latest_time',latest_certificate)
-    print('remaining_certificate',remaining_certificate)
-    delete_certi=remaining_certificate[0].split("/")[-1]
-    patch_require_certi.append(certificate_99)
-    patch_require_certi.append(latest_certificate)
-    print('delete required',delete_certi)
-    
-    
-    
-    
-    print('final certi status check',latest_certificate.split("/")[-1])    
-    certi_status = get_ssl_certi(certificate_name=latest_certificate.split("/")[-1])
+    full_url_certi.append(certificate_99)
+    full_url_certi.append(latest_certificate)
+    certi_status = get_ssl_certi(latest_certificate.split("/")[-1])
     print(certi_status)
+    try:
+        if certi_status == 'PROVISIONING' :
+        
+            finger_print=create_delete_https_proxy_get()
+            create_delete_patch_lb_front_end(certilist=full_url_certi,fingerprint=finger_print[0])     
+            time.sleep(10)
+            for i in remaining_certificate:
+                print(i)
+                certificate_name=i.split("/")[-1]
+                create_delete_ssl_delete(certificate_name)
+                print('delete certi',remaining_certificate)
+
+        else:
+            print('There is no new certificate is create')
+    except Exception as e:
+        print(e)
+
+    return 'Function run well'
+
+def create_delete_https_proxy_get():
+    print('i am proxy get under')
+
+    client = compute_v1.TargetHttpsProxiesClient()
     
-    if certi_status == 'PROVISIONING':
-        finger_print=https_proxy_get(load_balancer=lb)
-        print('get proxy')
-        patch_lb_front_end(load_balancer=lb,fingerprint=finger_print[0])
-        print('update proxy')
-        time.sleep(5)
-        
-        
-        urlmap_get(lb=lb) #give static lb name
-        print('url get and update path')
-        time.sleep(5)
-        get_backend_service()
-        print('get backend')
-        time.sleep(10)
-        backend_delete(backend_service_name=be_name[0])
-        time.sleep(10)
-        print('delete backend')
-        get_neg_list()
-        time.sleep(10)
-        print('get neg')
-
-        neg_delete()
-        time.sleep(10)
-        print('delete neg')
-        ssl_delete(certificate_name=delete_certi)
-        print('All items deleted sucessfully')
-        print(received_message)
-        subscriber.acknowledge(
-        subscription=subscription_path,
-        ack_ids=[received_message.ack_id],
-            )     
-
-
-
-
-
-            # Deletion code start
-
-
-    #         print('Our code work fine')
-    else:
-        print('The new certificate is not Active yet ')
-
-    # else:
-    #     print('Not required for delete operation because only one certificate is created')
-
-
-
-    return {"message": "complete the scan"}
-
-
-
-def get_ssl_certi(certificate_name:str):
-    client = compute_v1.SslCertificatesClient()
-
-    request = compute_v1.GetSslCertificateRequest(
-    project=project,
-    ssl_certificate=certificate_name,
-)
+    new_lb = lb
+    tar_proxy=str(new_lb+proxy_name)
+    # print("check",new_lb)
+    request = compute_v1.GetTargetHttpsProxyRequest(
+        project=project,
+        target_https_proxy=tar_proxy,
+    )
+    # Make the request
     response = client.get(request=request)
-    return response.managed.status
+    # print('whole_response',response)
+    certis = response.ssl_certificates
+    cd_certi_figer_print.append(response.fingerprint)
+   
+    return cd_certi_figer_print
 
+
+def create_delete_patch_lb_front_end(certilist:list,fingerprint:str):
+    print('i am under patch function')
+    client = compute_v1.TargetHttpsProxiesClient()
+    request_body={
+        # "creation_timestamp": "2023-11-07T19:31:29.652-08:00",
+        "fingerprint": fingerprint,
+        # "id": 5701903430321618942,
+        # "kind": "compute#targetHttpsProxy",
+        # "name": "server-side-test-lb-target-proxy-2",
+        # "quic_override": "NONE",
+        "ssl_certificates" :certilist
+        # [ 'https://www.googleapis.com/compute/v1/projects/tatvic-gcp-dev-team/global/sslCertificates/server-side-tagging-testing-certi2'],
+        # "url_map": "https://www.googleapis.com/compute/v1/projects/tatvic-gcp-dev-team/global/urlMaps/server-side-test-lb"
+
+    }
+    new_lb =lb   
+    tar_proxy=str(new_lb+proxy_name)
+    request = compute_v1.PatchTargetHttpsProxyRequest(
+        project=project,
+        target_https_proxy=tar_proxy,
+        target_https_proxy_resource=request_body    
+    )
+    response = client.patch(request=request)
+    print(response)
+    return response
+
+def create_delete_ssl_delete(certificate_name):
+    client = compute_v1.SslCertificatesClient()
+    
+
+    request = compute_v1.DeleteSslCertificateRequest(
+        project= project,
+        ssl_certificate= certificate_name
+    )
+   
+    try:
+        # Make the delete request
+        response = client.delete(request=request)
+        print(response)
+        print('certificate '+certificate_name +' deleted succesfully')
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
-    
-    # batch_function()
-
-
-# lb='test-lb-2'
-
-
-# proxy_name="-target-proxy"
-# my_list=[]
-
-# certi_fingerprint=[]
-# be_name=[]
-# neg_name=[]    
-
+    uvicorn.run("main:app", host="127.0.0.2", port=8080, reload=True)
+#
 #Loadbalancer get
 def https_proxy_get(load_balancer: str):
-    # print('proxy under',store_id[0])
+ 
     client = compute_v1.TargetHttpsProxiesClient()
-    # task_key = datastore_client.key('server-side-tagging', str(store_id[0]))
-    # task = datastore_client.get(task_key)
-    # json_data={
-    #     "certificate_name":task.get('certificate_name'),
-    # }
-
-    # certifinate_f_d=str(json_data['certificate_name'])
     new_lb = load_balancer
     tar_proxy=str(new_lb+proxy_name)
     # print("check",new_lb)
@@ -253,26 +252,14 @@ def https_proxy_get(load_balancer: str):
     print('whole_response',response)
     # certis = response.ssl_certificates
     certi_fingerprint.append(response.fingerprint)
-    # for cert in certis:
-    #     if certifinate_f_d in cert:
-    #         my_list.append(cert)
-    # print(certis)
     return certi_fingerprint
 
 
 def patch_lb_front_end(load_balancer:str,fingerprint:str):
     client = compute_v1.TargetHttpsProxiesClient()
     request_body={
-        # "creation_timestamp": "2023-11-07T19:31:29.652-08:00",
         "fingerprint": fingerprint,
-        # "id": 5701903430321618942,
-        # "kind": "compute#targetHttpsProxy",
-        # "name": "server-side-test-lb-target-proxy-2",
-        # "quic_override": "NONE",
         "ssl_certificates" :patch_require_certi
-        # [ 'https://www.googleapis.com/compute/v1/projects/tatvic-gcp-dev-team/global/sslCertificates/server-side-tagging-testing-certi2'],
-        # "url_map": "https://www.googleapis.com/compute/v1/projects/tatvic-gcp-dev-team/global/urlMaps/server-side-test-lb"
-
     }
     new_lb = load_balancer   
     tar_proxy=str(new_lb+proxy_name)
@@ -525,101 +512,12 @@ def ssl_delete(certificate_name):
 
 
 
-
-
-@app.get("/create-delete")
-
-def create_delete_batch():
-
-    full_url_certi = []
-    certificate_99,latest_certificate,remaining_certificate = DomainList.domain_list()
-    full_url_certi.append(certificate_99)
-    full_url_certi.append(latest_certificate)
-    certi_status = get_ssl_certi(latest_certificate.split("/")[-1])
-    print(certi_status)
-    try:
-        if certi_status == 'PROVISIONING' :
-        
-            finger_print=create_delete_https_proxy_get()
-            create_delete_patch_lb_front_end(certilist=full_url_certi,fingerprint=finger_print[0])     
-            time.sleep(10)
-            for i in remaining_certificate:
-                print(i)
-                certificate_name=i.split("/")[-1]
-                create_delete_ssl_delete(certificate_name)
-                print('delete certi',remaining_certificate)
-
-        else:
-            print('There is no new certificate is create')
-    except Exception as e:
-        print(e)
-
-
-
-
-
-    return 'Function run well'
-
-def create_delete_https_proxy_get():
-    print('i am proxy get under')
-
-    client = compute_v1.TargetHttpsProxiesClient()
-    
-    new_lb = lb
-    tar_proxy=str(new_lb+proxy_name)
-    # print("check",new_lb)
-    request = compute_v1.GetTargetHttpsProxyRequest(
-        project=project,
-        target_https_proxy=tar_proxy,
-    )
-    # Make the request
-    response = client.get(request=request)
-    # print('whole_response',response)
-    certis = response.ssl_certificates
-    cd_certi_figer_print.append(response.fingerprint)
-   
-    return cd_certi_figer_print
-
-
-def create_delete_patch_lb_front_end(certilist:list,fingerprint:str):
-    print('i am under patch function')
-    client = compute_v1.TargetHttpsProxiesClient()
-    request_body={
-        # "creation_timestamp": "2023-11-07T19:31:29.652-08:00",
-        "fingerprint": fingerprint,
-        # "id": 5701903430321618942,
-        # "kind": "compute#targetHttpsProxy",
-        # "name": "server-side-test-lb-target-proxy-2",
-        # "quic_override": "NONE",
-        "ssl_certificates" :certilist
-        # [ 'https://www.googleapis.com/compute/v1/projects/tatvic-gcp-dev-team/global/sslCertificates/server-side-tagging-testing-certi2'],
-        # "url_map": "https://www.googleapis.com/compute/v1/projects/tatvic-gcp-dev-team/global/urlMaps/server-side-test-lb"
-
-    }
-    new_lb =lb   
-    tar_proxy=str(new_lb+proxy_name)
-    request = compute_v1.PatchTargetHttpsProxyRequest(
-        project=project,
-        target_https_proxy=tar_proxy,
-        target_https_proxy_resource=request_body    
-    )
-    response = client.patch(request=request)
-    print(response)
-    return response
-
-def create_delete_ssl_delete(certificate_name):
+def get_ssl_certi(certificate_name:str):
     client = compute_v1.SslCertificatesClient()
-    
 
-    request = compute_v1.DeleteSslCertificateRequest(
-        project= project,
-        ssl_certificate= certificate_name
-    )
-   
-    try:
-        # Make the delete request
-        response = client.delete(request=request)
-        print(response)
-        print('certificate '+certificate_name +' deleted succesfully')
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    request = compute_v1.GetSslCertificateRequest(
+    project=project,
+    ssl_certificate=certificate_name,
+)
+    response = client.get(request=request)
+    return response.managed.status
--- main.py
+++ main.py
@@ -43,5 +43,5 @@ app.include_router(final.router,prefix='/sst',tags=['SSTmain'])
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="127.0.0.2", port=8080, reload=True)
 

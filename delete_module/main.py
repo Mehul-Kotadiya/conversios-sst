fb623bdb5c8a7d80f811e4c39f12fd32dc76c541
mehul <mehul@tatvic.com>
2023-12-27 17:32:28 +0530

    changes into create-delete module

--- delete_module/main.py
+++ delete_module/main.py
@@ -216,7 +216,7 @@ def create_delete_https_proxy_get():
    
     return cd_certi_figer_print
 
-def create_delete_patch_lb_front_end(certilist:list,fingerprint:str):
+async def create_delete_patch_lb_front_end(certilist:list,fingerprint:str):
     
     client = compute_v1.TargetHttpsProxiesClient()
     request_body={
